91da8c17c3ea3e82349c326aae072ef00ca63a52
Max Skorokhod <mrzandras@gmail.com>
2024-12-12 19:10:49 +0200

    content

---
+++ src/components/Hero.vue
@@ -0,0 +1,72 @@
+<script setup>
+import mainPhoto from "../assets/images/hero.svg";
+import { themeColor } from "../data/items";
+
+const heroHeading = "Всем привет, я в ахуевшем тильте";
+const buttonSeeFeature = "See Features";
+const buttonPricing = "Pricing";
+</script>
+
+<template>
+  <div class="untree_co-hero" id="home-section">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-12">
+          <div class="row align-items-center">
+            <div
+              class="col-lg-7 ml-auto order-lg-2"
+              data-aos="fade-right"
+              data-aos-delay="400"
+            >
+              <img :src="mainPhoto" alt="Image" class="img-fluid" />
+            </div>
+            <div class="col-lg-5">
+              <h1 class="heading" data-aos="fade-up" data-aos-delay="0">
+                {{ heroHeading }}
+              </h1>
+              <div class="excerpt" data-aos="fade-up" data-aos-delay="100">
+                <p>
+                  Старый бог -
+                  <span
+                    style="
+                      background-color: rgba(64, 123, 255, 0.07);
+                      color: #407bff;
+                    "
+                    >Максончик</span
+                  >
+                  Far far away, behind the word mountains, far from the
+                  countries Vokalia and Consonantia, there live the blind texts.
+                  Separated they live in Bookmarksgrove right at the coast
+                </p>
+              </div>
+              <p data-aos="fade-up" data-aos-delay="200">
+                <a
+                  href="#features-section"
+                  class="btn btn-primary smoothscroll"
+                  style="margin-right: 5px"
+                  :style="[
+                    { background: themeColor },
+                    { borderColor: themeColor },
+                  ]"
+                  >{{ buttonSeeFeature }}</a
+                >
+                <a
+                  href="#pricing-section"
+                  class="btn btn-outline-primary smoothscroll pricing"
+                  :style="[{ color: themeColor }, { borderColor: themeColor }]"
+                  >{{ buttonPricing }}</a
+                >
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.pricing:hover {
+  color: #fff !important;
+}
+</style>
